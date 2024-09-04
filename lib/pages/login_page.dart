aef3e85f4c1b6752e49a6755ce963b0ad1709eab
gobannaz <156826820+anna-banna@users.noreply.github.com>
2024-09-04 15:54:57 -0400

    pixel overflow fix

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -15,76 +15,79 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
         backgroundColor: Colors.grey[200],
         body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 100),
-                //logo
-                // to be added later, text for now
-                const Text(
-                  'Lookupp',
-                  style: TextStyle(
-                      fontSize: 50,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue),
-                ),
+          child: SingleChildScrollView(
+            physics: const NeverScrollableScrollPhysics(),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 100),
+                  //logo
+                  // to be added later, text for now
+                  const Text(
+                    'Lookupp',
+                    style: TextStyle(
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue),
+                  ),
 
-                const SizedBox(height: 75),
+                  const SizedBox(height: 75),
 
-                //email field
-                MyTextField(
-                  controller: emailController,
-                  hintText: 'Email',
-                  obscureText: false,
-                ),
+                  //email field
+                  MyTextField(
+                    controller: emailController,
+                    hintText: 'Email',
+                    obscureText: false,
+                  ),
 
-                const SizedBox(height: 10),
-                //password field
-                MyTextField(
-                  controller: passwordController,
-                  hintText: 'Password',
-                  obscureText: true,
-                ),
+                  const SizedBox(height: 10),
+                  //password field
+                  MyTextField(
+                    controller: passwordController,
+                    hintText: 'Password',
+                    obscureText: true,
+                  ),
 
-                const SizedBox(height: 10),
-                //forgot password
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
+                  const SizedBox(height: 10),
+                  //forgot password
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Forgot Password?',
+                          style: TextStyle(color: Colors.grey[600]),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  const SizedBox(height: 25),
+                  //sign in button
+                  MyButton(
+                    onTap: signIn,
+                  ),
+                  const SizedBox(height: 175),
+                  //sign up
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Text(
-                        'Forgot Password?',
+                        'Don\'t have an account?',
                         style: TextStyle(color: Colors.grey[600]),
                       ),
+                      const SizedBox(width: 4),
+                      const Text(
+                        'Sign up',
+                        style: TextStyle(
+                            color: Colors.blue, fontWeight: FontWeight.bold),
+                      )
                     ],
                   ),
-                ),
-
-                const SizedBox(height: 25),
-                //sign in button
-                MyButton(
-                  onTap: signIn,
-                ),
-                const SizedBox(height: 175),
-                //sign up
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Don\'t have an account?',
-                      style: TextStyle(color: Colors.grey[600]),
-                    ),
-                    const SizedBox(width: 4),
-                    const Text(
-                      'Sign up',
-                      style: TextStyle(
-                          color: Colors.blue, fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ));
