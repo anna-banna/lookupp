67b773046471a51193663df0940884b4128cd937
gobannaz <156826820+anna-banna@users.noreply.github.com>
2024-09-03 22:06:08 -0400

    completed login ui

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:lookupp/components/dynamic_textfield.dart';
+import 'package:lookupp/components/login_button.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
 
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+
+  void signIn() {}
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +17,7 @@ class LoginPage extends StatelessWidget {
         body: SafeArea(
           child: Center(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const SizedBox(height: 100),
                 //logo
@@ -26,7 +30,7 @@ class LoginPage extends StatelessWidget {
                       color: Colors.blue),
                 ),
 
-                const SizedBox(height: 100),
+                const SizedBox(height: 75),
 
                 //email field
                 MyTextField(
@@ -41,12 +45,45 @@ class LoginPage extends StatelessWidget {
                   controller: passwordController,
                   hintText: 'Password',
                   obscureText: true,
-                )
+                ),
+
+                const SizedBox(height: 10),
                 //forgot password
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Forgot Password?',
+                        style: TextStyle(color: Colors.grey[600]),
+                      ),
+                    ],
+                  ),
+                ),
 
+                const SizedBox(height: 25),
                 //sign in button
-
+                MyButton(
+                  onTap: signIn,
+                ),
+                const SizedBox(height: 175),
                 //sign up
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Don\'t have an account?',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                    const SizedBox(width: 4),
+                    const Text(
+                      'Sign up',
+                      style: TextStyle(
+                          color: Colors.blue, fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
               ],
             ),
           ),
